176a2c76e4e723cb713e825c865f4415e7bdf857
lena <elen.smirnova.2001@yandex.ru>
2024-05-10 21:36:38 +0300

    end first lesson

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        int integer = 123;
+        if (integer < 1) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+        int temp = 0;
+        temp = temp + 1;
+        temp += 1;
+        temp++;
+        System.out.println("temp = " + temp);
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = " + i);
+        }
         System.out.println("Hello world!");
     }
 }
